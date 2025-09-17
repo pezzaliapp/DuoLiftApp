a6e0187497fbe7091d14f04d9614121c89703eb4
pezzaliapp <pezzalialessandro@gmail.com>
2025-09-17 14:17:23 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,2 @@
-const CACHE = 'duoliftapp-v3';
-const ASSETS = [
-  './',
-  './index.html',
-  './app.js',
-  './manifest.json',
-  './readme.html',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE && caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
-  if (url.origin === location.origin){
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-  } else {
-    e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));
-  }
-});
+self.addEventListener('install', e=>self.skipWaiting());
+self.addEventListener('activate', e=>clients.claim());
